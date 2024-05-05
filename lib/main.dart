e0bf50e5b14a15b1a36afaa5d17a45447d1898cf
ayb23nee <aygerim.budaychieva.nee@fh-zwickau.de>
2024-05-05 14:43:16 +0200

    refactored the routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
@@ -17,9 +18,9 @@ void main() {
       ),
       home: const HomePage(),
       routes: {
-        '/login/': (context) => const LoginView(),
-        '/register/': (context) => const RegisterView(),
-        '/notes/': (context) => const NotesView()
+        loginRoute : (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView()
       }));
 }
 
@@ -45,7 +46,6 @@ class HomePage extends StatelessWidget {
               } else {
                 return const LoginView();
               }
-              return const Text('Done');
               // final emailVerified = user?.emailVerified ?? false;
               // if (emailVerified) {
               //   return const Text('Done');
@@ -65,7 +65,6 @@ class HomePage extends StatelessWidget {
               //   // });
               //   return const VerifyEmailView();
               // }
-              return const LoginView();
             default:
               return const Text('Loading');
           }
@@ -91,7 +90,7 @@ class NotesView extends StatelessWidget {
                   final shouldLogout = await showLogoutDialog(context);
                   if(shouldLogout){
                     await FirebaseAuth.instance.signOut();
-                    Navigator.of(context).pushNamedAndRemoveUntil('/login/', (route) => false);
+                    Navigator.of(context).pushNamedAndRemoveUntil(loginRoute, (route) => false);
                   }
               }
             },
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
+import 'package:mynotes/constants/routes.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
@@ -63,7 +65,7 @@ class _LoginViewState extends State<LoginView> {
                 );
                 // devtools.log(userCredential.toString());
                 Navigator.of(context).
-                  pushNamedAndRemoveUntil('/notes/', (route) => false);
+                  pushNamedAndRemoveUntil(notesRoute, (route) => false);
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
                   devtools.log('User not found');
@@ -76,7 +78,7 @@ class _LoginViewState extends State<LoginView> {
           ),
           TextButton(
             onPressed: (){
-              Navigator.of(context).pushNamedAndRemoveUntil('/register/', (route) => false);
+              Navigator.of(context).pushNamedAndRemoveUntil(registerRoute, (route) => false);
             },
             child: const Text('Not registered yet? Register here!'
             )
